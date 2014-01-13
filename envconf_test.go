01b4df6839bf4778e6c21a3bd0e2f167d69cfc74
moshee <moshee@displaynone.us>
2014-01-13 10:20:07 -0800

    more envconf tests

--- envconf_test.go
+++ envconf_test.go
@@ -2,7 +2,9 @@ package gas
 
 import (
 	"os"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestEnvConf(t *testing.T) {
@@ -34,4 +36,59 @@ func TestEnvConf(t *testing.T) {
 		t.Errorf("Expected default value PORT = 80, got %d", Env.PORT)
 	}
 	os.Setenv("GAS_PORT", env["GAS_PORT"])
+
+	conf := struct {
+		BOOL     bool
+		STRING   string
+		INT      int
+		INT64    int64
+		UINT     uint
+		UINT64   uint64
+		FLOAT64  float64
+		DURATION time.Duration
+	}{}
+
+	env = map[string]string{
+		"BOOL":     "t",
+		"STRING":   "testing",
+		"INT":      "9553325",
+		"INT64":    "-3453466699214",
+		"UINT":     "22299929",
+		"UINT64":   "9325324234324324",
+		"FLOAT64":  "3.14159265358979323846264833",
+		"DURATION": "1h2s3ms4µs5ns",
+	}
+
+	prefix := "GAS_TEST_"
+
+	for key := range env {
+		os.Setenv(prefix+key, "_")
+	}
+
+	if err := EnvConf(&conf, prefix); err == nil {
+		t.Errorf("Expected some sort of error, got nothing")
+	}
+
+	for key, val := range env {
+		os.Setenv(prefix+key, val)
+	}
+
+	if err := EnvConf(&conf, prefix); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	assertEqual := func(a, b interface{}) {
+		if !reflect.DeepEqual(a, b) {
+			t.Errorf("Expected %v (%[1]T) != got %v (%[2]T)", b, a)
+		}
+	}
+
+	assertEqual(conf.BOOL, true)
+	assertEqual(conf.STRING, "testing")
+	assertEqual(conf.INT, 9553325)
+	assertEqual(conf.INT64, int64(-3453466699214))
+	assertEqual(conf.UINT, uint(22299929))
+	assertEqual(conf.UINT64, uint64(9325324234324324))
+	assertEqual(conf.FLOAT64, 3.14159265358979323846264833)
+	assertEqual(conf.DURATION, time.Hour+2*time.Second+3*time.Millisecond+4*time.Microsecond+5*time.Nanosecond)
 }
