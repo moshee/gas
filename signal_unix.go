1bba4bdeef0b9889e6c8afc2a08ddbc1ac1b9001
moshee <moshee@displaynone.us>
2014-01-24 23:28:01 -0800

    signals

--- signal_unix.go
+++ signal_unix.go
@@ -10,7 +10,8 @@ import (
 var signal_funcs = map[os.Signal]func(){
 	syscall.SIGINT:  stop,
 	syscall.SIGQUIT: stop,
-	syscall.SIGHUP: func() {
+	syscall.SIGTERM: stop,
+	syscall.SIGUSR1: func() {
 		ts := parse_templates(template_dir)
 		Templates = ts
 		LogNotice("Templates reloaded.")
